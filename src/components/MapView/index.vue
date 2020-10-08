ca17b0ca0ea76895173c7e0af92cbfc07cc3cb79
lml <soalin228@163.com>
2020-10-08 09:57:50 +0800

    feat: liquidfill

--- src/components/MapView/index.vue
+++ src/components/MapView/index.vue
@@ -9,16 +9,39 @@
     <div class="left">
       <b-map-scatter/>
     </div>
+    <div class="right">
+      <el-card shadow="hover">
+        <template v-slot:header>
+          <div class="title">用户月同比增长</div>
+        </template>
+        <template>
+          <div class="chart-wrapper">
+            <liquidfill/>
+          </div>
+        </template>
+      </el-card>
+      <el-card shadow="hover">
+        <template v-slot:header>
+          <div class="title">热门搜索</div>
+        </template>
+        <template>
+          <div class="chart-wrapper">
+          </div>
+        </template>
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script>
 import BMapScatter from '../BMapScatter'
+import Liquidfill from '../LiquidFill'
 
 export default {
   name: 'MapView',
   components: {
-    BMapScatter
+    BMapScatter,
+    Liquidfill
   }
 }
 </script>
@@ -32,6 +55,30 @@ export default {
     flex: 0 0 80%;
     width: 80%;
     height: 600px;
+  }
+
+  .right {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-left: 20px;
+
+    .title {
+      font-size: 14px;
+      font-weight: 500;
+      display: flex;
+      align-items: center;
+      box-sizing: border-box;
+      height: 60px;
+      padding: 0 0 0 20px;
+      border-bottom: 1px solid #eee;
+    }
+
+    .chart-wrapper {
+      width: 100%;
+      height: 190px;
+    }
   }
 }
 </style>
